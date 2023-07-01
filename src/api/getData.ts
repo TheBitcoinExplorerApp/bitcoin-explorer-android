bb5a28bd418893e363a8177c3f4749f7c096dffb
PedroW21 <pedrow21br@gmail.com>
2023-06-30 22:20:07 -0300

    getBlocks data function

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,8 +1,15 @@
+import { BlockInfoType } from "src/components/BlocksInfo/types";
 import { FeesType } from "src/components/PrioritiesTax/types";
-import { taxTransactions } from "src/env/apiLinks";
+import { taxTransactions, blocksInfo } from "src/env/apiLinks";
 
 export const getTaxes = async () => {
     const response = await fetch(taxTransactions);
     const data: FeesType = await response.json();
+    return data;
+}
+
+export const getBlocks = async () => {
+    const response = await fetch(blocksInfo);
+    const data: BlockInfoType[] = await response.json();
     return data;
 }

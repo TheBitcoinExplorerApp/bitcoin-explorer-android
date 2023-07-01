9273136174d01c417476500c1422232b70f8f56e
PedroW21 <pedrow21br@gmail.com>
2023-06-30 21:30:44 -0300

    created a single file do export api links

---
+++ src/env/apiLinks.ts
@@ -0,0 +1,14 @@
+export const taxTransactions = 'https://mempool.space/api/v1/fees/recommended';
+
+export const blocksInfo = 'https://mempool.space/api/v1/blocks/';
+
+export const lastTenTransactions = 'https://mempool.space/api/mempool/recent';
+
+export const blocksTransaction =
+  'https://mempool.space/api/block/block_hash/txs';
+
+export const transactionInfo = 'https://mempool.space/api/tx/';
+
+export const addressInfo = 'https://mempool.space/api/address/';
+
+export const addressTransactions = 'https://mempool.space/api/address/bitcoin_address/txs/chain';

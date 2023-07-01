7442bece14d377ea92e9c85a0944ddc375ded5e3
PedroW21 <pedrow21br@gmail.com>
2023-06-30 22:20:45 -0300

    created mock folder and initialStates file to storage initialStates there

---
+++ src/mocks/initialStates.ts
@@ -0,0 +1,30 @@
+export const initialStateBlocks = [
+  {
+    blockHeight: 1,
+    satPerVbyte: '1',
+    size: '1',
+    transactions: 1,
+    timeAgo: '1',
+  },
+  {
+    blockHeight: 2,
+    satPerVbyte: '1',
+    size: '1',
+    transactions: 1,
+    timeAgo: '1',
+  },
+  {
+    blockHeight: 3,
+    satPerVbyte: '1',
+    size: '1',
+    transactions: 1,
+    timeAgo: '1',
+  },
+  {
+    blockHeight: 4,
+    satPerVbyte: '1',
+    size: '1',
+    transactions: 1,
+    timeAgo: '1',
+  },
+];

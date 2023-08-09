4150875b13a5dadec6fe69fdaef0371abfe32930
PedroW21 <pedrow21br@gmail.com>
2023-08-08 21:19:49 -0300

    Moved BlockModal to your respective folder

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { ModalProps } from './types';
 import TransactionModal from './components/TransactionModal/TransactionModal';
-import BlockModal from './components/BlockModal';
+import BlockModal from './components/BlockModal/BlockModal';
 
 export default function Modal(props: ModalProps) {
   const { keyForSearch, modalType, isVisible, handleModalClose } = props;

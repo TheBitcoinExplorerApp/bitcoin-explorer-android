aff8e3f5a3959a278c5781f0a4139adb8499216b
PedroW21 <pedrow21br@gmail.com>
2023-09-04 20:25:13 -0300

    feat: search only accepts chars and numbers, not special chars

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,27 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
 
 export default function Search() {
+  const [searchContent, setSearchContent] = React.useState("");
+
+  useEffect(() => {}, [searchContent]);
+
+  const onlyLettersAndNumbers = /^[a-zA-Z0-9]*$/;
+
+  const handleInputChange = (text: string) => {
+    if (onlyLettersAndNumbers.test(text)) {
+      setSearchContent(text);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
         placeholder="Pesquisar"
         style={styles.input}
         placeholderTextColor="#8d8d9a"
+        value={searchContent}
+        onChangeText={handleInputChange}
       />
     </View>
   );
@@ -15,14 +29,14 @@ export default function Search() {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    width: "100%",
   },
   input: {
-    backgroundColor: '#1d2133',
+    backgroundColor: "#1d2133",
     borderRadius: 5,
     padding: 12,
-    width: '100%',
+    width: "100%",
     fontSize: 18,
-    color: '#FFF',
+    color: "#FFF",
   },
 });
